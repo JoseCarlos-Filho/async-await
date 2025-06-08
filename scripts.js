9837ec9f1a91a7fd6f8a360707a037a883a4131b
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-07 21:43:42 -0300

    feat: evento de mudança(change) no elemento ao selecionar um arquivo executa função assincrona.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ uploadBtn.addEventListener("click", () => {
   inputUpload.click();
 });
 
+// função que devolve uma Promisse ao carregar um arquivo
 function lerConteudoDoArquivo(arquivo) {
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
@@ -19,3 +20,24 @@ function lerConteudoDoArquivo(arquivo) {
         leitor.readAsDataURL(arquivo);
     })
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+// elemento html com evento de mudança "change" chama uma função assincrona
+// com o parâmetro do arquivo selecionado e espera a promisse(await) resolver
+// se encontrou o arquivo selecionado ou não.
+// se encontrou atualiza source("src") do elemento e o nome do arquivo.
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivos = evento.target.files[0];
+
+    if (arquivos) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivos); 
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (error) {
+            console.error("Erro ao ler o arquivo:", error);
+        }
+    }
+});
